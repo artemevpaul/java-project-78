116c11125808ecd5358c3e346cac2b8f8fecc768
Pavel Artemev <damention@MacBook-Pro.local>
2024-10-15 15:09:14 +0200

    test fix

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -4,18 +4,18 @@ package hexlet.code.schemas;
 public class StringSchema extends BaseSchema<String> {
 
     public StringSchema required() {
-        addCheck(value -> value != null && !value.isEmpty());
+        addCheck(value -> !"".equals(value) && value != null);
         setRequired();
         return this;
     }
 
     public StringSchema minLength(int length) {
-        addCheck(value -> value != null && value.toString().length() >= length);
+        addCheck(value -> value != null && value.length() >= length);
         return this;
     }
 
     public StringSchema contains(String substring) {
-        addCheck(value -> value != null && value.toString().contains(substring));
+        addCheck(value -> value != null && value.contains(substring));
         return this;
     }
 }
