3da46cb29f5590fe580d8d3bf85d4532e63c096b
Pavel Artemev <damention@MacBook-Pro.local>
2024-10-17 00:05:49 +0200

    fixing shape method

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -2,7 +2,7 @@ package hexlet.code.schemas;
 
 import java.util.Map;
 
-public final class MapSchema extends BaseSchema<Map> {
+public final class MapSchema extends BaseSchema<Map<?, ?>> {
 
     public MapSchema required() {
         addCheck("REQUIRED", (value -> value != null));
@@ -11,22 +11,19 @@ public final class MapSchema extends BaseSchema<Map> {
     }
 
     public MapSchema sizeof(Integer n) {
-        addCheck("SIZE_OF", (map -> ((Map<?, ?>) map).size() == n));
+        addCheck("SIZE_OF", (map -> map.size() == n));
         return this;
     }
 
     public <T> MapSchema shape(Map<String, BaseSchema<T>> schemas) {
-        addCheck("SHAPE", (map -> {
-            for (Map.Entry<String, BaseSchema<T>> entry : schemas.entrySet()) {
-                String key = entry.getKey();
-                BaseSchema schema = entry.getValue();
-
-                if (!map.containsKey(key) || !schema.isValid(map.get(key))) {
-                    return false;
-                }
-            }
-            return true;
-        }));
+        addCheck("SHAPE", (map ->
+                schemas.entrySet().stream()
+                        .allMatch(entry -> {
+                            String key = entry.getKey();
+                            BaseSchema schema = entry.getValue();
+                            return schema.isValid(map.get(key))
+                        })
+        ));
         return this;
     }
 }
